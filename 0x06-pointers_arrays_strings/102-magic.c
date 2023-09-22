b0f663411cd8f8614d54ae547dda3d2f9922ab17
abdulwe <ayodejiweb07@gmail.com>
2023-09-22 10:05:24 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print........
+ * @n: number to take address of
+ * @p: it is a pointer for n
+ * @a: it is an array
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+/**
+ * write your line of code here...
+ * Remember
+ * -you are allow to use a
+ * -you are not allow to modify p
+ * -only one statement
+ * -you are allow to code anything else than this line of code
+ */
+
+*(p + 5) = 98;
+/* ...so that this print 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
+
