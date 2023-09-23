3d10bf79a61745e84234650101b316aceb5e1052
abdulwe <ayodejiweb07@gmail.com>
2023-09-22 19:10:31 -0700

    commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -23,6 +23,7 @@ void rev_string(char* n)
 		*(n + j) = *(n + i);
 		*(n + i) = temp;
 	}
+}
 
 	/**
 	 * infinite_add - add 2 numbers together
@@ -33,10 +34,10 @@ void rev_string(char* n)
 	 * Return: pointer to calling function
 	 */
 
-	char* infinite_add(char *n1, char *n2, char *r, int size_r)
+	char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	{
 		int overflow = 0, i = 0, j = 0, digits =0;
-		int val1=0, val2 = 0, temp_tot = 0;
+		int val1 = 0, val2 = 0, temp_tot = 0;
 
 		while (*(n1 + i) != '\0')
 			i++;
@@ -50,7 +51,7 @@ void rev_string(char* n)
 		while (j >= 0 || i >= 0 || overflow == 1)
 		{
 			if (i < 0)
-				val1 - 0;
+				val1 = 0;
 			else 
 				val1 = *(n1 + i) - '0';
 			if (j < 0)
@@ -74,7 +75,7 @@ void rev_string(char* n)
 		*(r + digits) = '\0';
 		rev_string(r);
 		return (r);
-	}
+}
 
 
 
